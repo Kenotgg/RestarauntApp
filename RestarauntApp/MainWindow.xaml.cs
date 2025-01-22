1867c357f96e3c5533c157cab0ce248b6a980fe3
Kenotgg <130081795+Kenotgg@users.noreply.github.com>
2025-01-23 00:06:50 +0500

    Nive

--- RestarauntApp/MainWindow.xaml.cs
+++ RestarauntApp/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Query;
 namespace RestarauntApp
 {
     /// <summary>
@@ -18,36 +20,137 @@ namespace RestarauntApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<ListBox> listBoxes = new List<ListBox>();
         public MainWindow()
         {
             InitializeComponent();
             GetData();
         }
 
-        
+
 
         void GetData()
         {
             using (RestarauntContext db = new RestarauntContext())
             {
 
-              
-                var dishList = from dish in db.Dishes
-                               join singleDishList in db.DishLists on dish.Id equals singleDishList.DishId
-                               join ingridient in db.Ingridients on singleDishList.IngridientId equals ingridient.Id
-                               select new
-                               {
-                                   ID = singleDishList.Id,
-                                   DishTitle = dish.Title,
-                                   IngridietnTitle = ingridient.Title,
-                                   CountOfIngridients = singleDishList.Count
-                               };
+                //Выборка для списка блюд
+                //var dishList = from dish in db.Dishes
+                //               join singleDishList in db.DishLists on dish.Id equals singleDishList.DishId
+                //               join ingridient in db.Ingridients on singleDishList.IngridientId equals ingridient.Id
+                //               select new
+                //               {
+                //                   ID = singleDishList.Id,
+                //                   DishTitle = dish.Title,
+                //                   IngridietnTitle = ingridient.Title,
+                //                   CountOfIngridients = singleDishList.Count
+                //               };
 
-                foreach (var d in dishList)
+                var dishes = db.Dishes.Select(p => new
                 {
-                    MainListBox.Items.Add(d);
+                    Title = p.Title,
+                    Weight = p.Weight,
+                    Kcal = p.KiloCalories,
+                    Status = p.Status,
+                    Proteins = p.Proteins,
+                    Fats = p.Fats,
+                    Cabohydrates = p.Carbohydrates
+                }
+                );
+               
+                listBoxes.Add(ListBox1);
+                listBoxes.Add(ListBox2);
+                listBoxes.Add(ListBox3);
+                listBoxes.Add(ListBox4);
+                listBoxes.Add(ListBox5);
+                listBoxes.Add(ListBox6);
+                int indexer = 0;
+                foreach (var d in dishes)
+                {
+                    
+                    if (listBoxes[indexer].Items.Count < 4)
+                    {
+                        listBoxes[indexer].Items.Add($"Название: {d.Title}");
+                        listBoxes[indexer].Items.Add($"Вес: {d.Weight}");
+                        listBoxes[indexer].Items.Add($"ККАЛ: {d.Kcal}");
+                        listBoxes[indexer].Items.Add($"БЖУ: {d.Proteins} {d.Fats} {d.Cabohydrates}");
+                    }
+                    
+                    if (indexer <= listBoxes.Count)
+                    {
+                        indexer++;
+                    }
+
+                }
+                for (int i = 0; i < listBoxes.Count; i++)
+                {
+                    if (listBoxes[i].Items.Count < 4)
+                    {
+                        if(listBoxes[i] != null) 
+                        {
+                            AprovedPanel.Children.Remove(listBoxes[i]);
+                            if(listBoxes[i] != null) 
+                            {
+                                NotAprovedPanel.Children.Remove(listBoxes[i]);
+                            }
+                        }
+                        
+                    }
                    
                 }
+            }
+
+          
+            
+        }
+
+        private void onRightButton(object sender, RoutedEventArgs e)
+        {
+            int indexer = 0;
+
+            foreach (var l in listBoxes)
+            {
+                
+               
+                    if (listBoxes[indexer].SelectedItem != null && listBoxes[indexer].Parent != NotAprovedPanel)
+                    {
+                        
+                        AprovedPanel.Children.Remove(listBoxes[indexer]);
+                        NotAprovedPanel.Children.Add(listBoxes[indexer]);
+                        listBoxes[indexer].SelectedItem = null;
+                        
+                    }
+                if (indexer <= listBoxes.Count)
+                {
+                    indexer++;
+                }
+                
+                
+            }
+            
+            
+        }
+
+        private void onLeftButton(object sender, RoutedEventArgs e)
+        {
+            int indexer = listBoxes.Count-1;
+
+            foreach (var l in listBoxes)
+            {
+
+
+                if (listBoxes[indexer].SelectedItem != null && listBoxes[indexer].Parent != AprovedPanel)
+                {
+                    NotAprovedPanel.Children.Remove(listBoxes[indexer]);
+                    AprovedPanel.Children.Add(listBoxes[indexer]);
+                    listBoxes[indexer].SelectedItem = null;
+
+                }
+                if (indexer >= 0)
+                {
+                    indexer--;
+                }
+
 
             }
         }
